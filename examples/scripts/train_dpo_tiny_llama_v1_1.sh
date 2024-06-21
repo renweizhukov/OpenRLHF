34564cce0d05859560e86090fe40050c4efa3b93
Wei Ren <wrn@amazon.com>
2024-06-21 00:40:15 -0700

    Add a DPO training script for TinyLlama v1.1
    https://huggingface.co/TinyLlama/TinyLlama_v1.1
    
    Signed-off-by: Wei Ren <wrn@amazon.com>

---
+++ examples/scripts/train_dpo_tiny_llama_v1_1.sh
@@ -0,0 +1,32 @@
+set -x 
+
+# We only changed "--save_path" and "--pretrain" in train_dpo_llama.sh.
+read -r -d '' training_commands <<EOF
+../train_dpo.py \
+     --save_path ./ckpt/tiny_llama_v1.1 \
+     --save_steps -1 \
+     --logging_steps 1 \
+     --eval_steps -1 \
+     --train_batch_size 128 \
+     --micro_train_batch_size 1 \
+     --pretrain TinyLlama/TinyLlama_v1.1 \
+     --bf16 \
+     --max_epochs 1 \
+     --max_len 2048 \
+     --zero_stage 3 \
+     --beta 0.1 \
+     --learning_rate 5e-7 \
+     --dataset Anthropic/hh-rlhf,tasksource/oasst1_pairwise_rlhf_reward,lmsys/chatbot_arena_conversations,openai/webgpt_comparisons \
+     --dataset_probs 0.72,0.08,0.12,0.08 \
+     --flash_attn \
+     --gradient_checkpointing \
+     --ref_offload
+EOF
+     # --wandb [WANDB_TOKENS] or True (use wandb login command)
+     # --ipo [for IPO]
+     # --label_smoothing 0.1 [for cDPO]
+
+
+if [[ ${1} != "slurm" ]]; then
+    deepspeed $training_commands
+fi
